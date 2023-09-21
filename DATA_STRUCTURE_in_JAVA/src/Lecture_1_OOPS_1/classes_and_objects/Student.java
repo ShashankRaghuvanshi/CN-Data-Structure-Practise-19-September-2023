39a4ca040aa6902623f4c687c98bc957f86091ef
Shashank <ishashankraghuvanshi@gmail.com>
2023-09-21 08:34:21 +0530

    Initial Commit

---
+++ DATA_STRUCTURE_in_JAVA/src/Lecture_1_OOPS_1/classes_and_objects/Student.java
@@ -0,0 +1,7 @@
+package Lecture_1_OOPS_1.classes_and_objects;
+
+public class Student 
+{
+    public String name;
+    public int rollNumber;
+}
---
+++ DATA_STRUCTURE_in_JAVA/src/Lecture_1_OOPS_1/temp/StudentUse2.java
@@ -0,0 +1,13 @@
+package Lecture_1_OOPS_1.temp;
+
+import Lecture_1_OOPS_1.classes_and_objects.Student;
+
+public class StudentUse2 
+{
+    public static void main(String[] args) 
+    {
+        Student s1 = new Student();
+        s1.name = "Shashank";
+        s1.rollNumber = 39;
+    }
+}
