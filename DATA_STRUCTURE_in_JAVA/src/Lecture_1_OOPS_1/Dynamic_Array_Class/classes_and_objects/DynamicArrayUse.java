3ed707763a2e699a067fdc7d9d44e5942a518801
Shashank <ishashankraghuvanshi@gmail.com>
2023-09-23 08:15:08 +0530

    Initial Commit

---
+++ DATA_STRUCTURE_in_JAVA/src/Lecture_1_OOPS_1/Dynamic_Array_Class/classes_and_objects/DynamicArrayUse.java
@@ -0,0 +1,27 @@
+package Lecture_1_OOPS_1.Dynamic_Array_Class.classes_and_objects;
+
+public class DynamicArrayUse 
+{
+    public static void main(String[] args) 
+    {
+        DynamicArray d = new DynamicArray();
+
+    for(int i=0; i<100; i++)
+    {
+        d.add(i+10);
+    }
+    
+    System.out.println(d.size());
+
+    d.set(4,10);
+    System.out.println(d.get(3));
+    System.out.println(d.get(4));
+
+    while(!d.isEmpty())
+    {
+        System.out.println(d.removeLast());
+        System.out.println("size = "+d.size());
+    }
+
+    }
+}
