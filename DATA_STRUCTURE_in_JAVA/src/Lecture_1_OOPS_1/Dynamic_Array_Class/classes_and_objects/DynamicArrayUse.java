2f9770d1ce7a64cd73c5a771018e9b26da7a0155
Shashank <ishashankraghuvanshi@gmail.com>
2023-09-23 08:17:33 +0530

    Initial Commit

--- DATA_STRUCTURE_in_JAVA/src/Lecture_1_OOPS_1/Dynamic_Array_Class/classes_and_objects/DynamicArrayUse.java
+++ DATA_STRUCTURE_in_JAVA/src/Lecture_1_OOPS_1/Dynamic_Array_Class/classes_and_objects/DynamicArrayUse.java
@@ -6,22 +6,22 @@ public class DynamicArrayUse
     {
         DynamicArray d = new DynamicArray();
 
-    for(int i=0; i<100; i++)
-    {
-        d.add(i+10);
-    }
+        for(int i=0; i<100; i++)
+        {
+            d.add(i+10);
+        }
     
-    System.out.println(d.size());
+        System.out.println(d.size());
 
-    d.set(4,10);
-    System.out.println(d.get(3));
-    System.out.println(d.get(4));
+        d.set(4,10);
+        System.out.println(d.get(3));
+        System.out.println(d.get(4));
 
-    while(!d.isEmpty())
-    {
-        System.out.println(d.removeLast());
-        System.out.println("size = "+d.size());
-    }
+        while(!d.isEmpty())
+        {
+            System.out.println(d.removeLast());
+            System.out.println("size = "+d.size());
+        }
 
     }
 }
