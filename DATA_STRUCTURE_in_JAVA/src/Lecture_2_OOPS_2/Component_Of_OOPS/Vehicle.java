e9789fcd28fd87cac6d34a02aba66739f8e2ecad
Shashank <ishashankraghuvanshi@gmail.com>
2023-09-26 08:35:31 +0530

    Initial Commit

---
+++ DATA_STRUCTURE_in_JAVA/src/Lecture_2_OOPS_2/Component_Of_OOPS/Vehicle.java
@@ -0,0 +1,13 @@
+package Lecture_2_OOPS_2.Component_Of_OOPS;
+
+public class Vehicle 
+{
+    String color;
+    int maxSpeed;
+
+    public void print()
+    {
+        System.out.println("Vehicle color: "+ color);
+        System.out.println("Vehicle maxSpeed: "+ maxSpeed);
+    }
+}
