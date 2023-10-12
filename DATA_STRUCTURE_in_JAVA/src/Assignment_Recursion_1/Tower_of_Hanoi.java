f9fee67b59aa294985ab34ef52bcc29e43c624dc
Shashank <ishashankraghuvanshi@gmail.com>
2023-10-12 23:30:27 +0530

    Initial Commit

--- DATA_STRUCTURE_in_JAVA/src/Assignment_Recursion_1/Tower_of_Hanoi.java
+++ DATA_STRUCTURE_in_JAVA/src/Assignment_Recursion_1/Tower_of_Hanoi.java
@@ -1,6 +1,24 @@
 package Assignment_Recursion_1;
+import java.util.Scanner;
 
 public class Tower_of_Hanoi 
 {
-    
+    public static void towerOfHanoi(int disks, char source, char auxiliary, char destination) 
+    {
+		if(disks == 0){
+            return;
+        }
+        towerOfHanoi(disks-1, source, destination, auxiliary);
+        System.out.println(source+ " "+ destination);
+        towerOfHanoi(disks-1, auxiliary, source, destination); 
+	}
+
+    public static void main(String[] args) 
+    {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        towerOfHanoi(n, 'a', 'b', 'c');
+
+        s.close();
+    }
 }
